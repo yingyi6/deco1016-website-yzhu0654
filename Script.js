996ebb6336d4383cf807a499ee7c9168251269b9
yingyi6 <57380909+yingyi6@users.noreply.github.com>
2019-11-09 12:32:32 +1100

    intial commit

---
+++ Script.js
@@ -0,0 +1,21 @@
+
+// Get the modal for signing up
+var modal = document.getElementById('modal-signup');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// Get the modal for loging in 
+var modal = document.getElementById('modal-login');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
