c66de49b7f083b033f112b11e9f54c421f3c1a1c
yingyi6 <57380909+yingyi6@users.noreply.github.com>
2019-11-11 02:37:38 +1100

    modal changes

--- Script.js
+++ Script.js
@@ -1,21 +1,19 @@
 
-// Get the modal for signing up
-var modal = document.getElementById('modal-signup');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
 // Get the modal for loging in 
-var modal = document.getElementById('modal-login');
+var modalA = document.getElementById('modal-login');
+
+// Get the modal for signing up
+var modalB = document.getElementById('modal-signup');
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalB) {
+    modalB.style.display = "none";
+  }
+  if (event.target == modalA) {
+    modalA.style.display = "none";
   }
 }
+
+
